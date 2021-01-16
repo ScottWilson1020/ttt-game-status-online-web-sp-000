584850961e34e5823c00cbdf00b3a2f1cd9798e1
scottwilson1020 <scottwilson1020@gmail.com>
2021-01-15 19:11:40 -0600

    commit tree b742f1702a2acbb7bf8e1e8ed61c022a2821ab77

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,5 +1,5 @@
 
-
+require 'pry'
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -33,7 +33,7 @@ def won?(board)
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
-
+    
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
@@ -42,6 +42,7 @@ def won?(board)
   }
   return false
 end
+
 
 def full?(board)
   board.all? {|index| index == "X" || index == "O"}
